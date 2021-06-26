1567a32d0dc1010c825b223c8a669667825b765f
Radhika <radhika.shankar1220@gmail.com>
2021-06-26 09:48:53 +0530

    FIRST COMMIT

---
+++ Rectangle/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Class1
+    {
+        public static void Rectangle()
+        {
+            int length;
+            int breadth;
+
+        }
+        public int calcualteArea(int length, int breadth)
+        {
+            return length * breadth;
+        }
+        public static void Calculate()
+        {
+            Console.WriteLine("calculating area..");
+        }
+
+    }
+}
+
---
+++ Rectangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+
+            int a = 1;
+            string var = "my first variable";
+            Console.WriteLine(var + a);
+            Console.WriteLine("Hello World!");
+
+            int length = Convert.ToInt32(Console.ReadLine());
+            int breadth = Convert.ToInt32(Console.ReadLine());
+
+            Class1 rect = new Class1();
+            Class1.Calculate();
+
+            Console.WriteLine(rect.calcualteArea(length, breadth));
+
+            Console.ReadKey(true);
+        }
+    }
+}
